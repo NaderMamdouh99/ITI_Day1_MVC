23ad11201db1dce01b8afb97882767d6e01781fc
NaderMamdouh99 <Nader.mamdouh9898@gmail.com>
2023-12-16 18:28:55 +0200

    Add project files.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Day1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1.Controllers
+{
+    public class FirstController : Controller
+    {
+        static List<Course> CoursesData = new List<Course>()
+        {
+            new Course(){Id = 1, Name ="Css"},
+            new Course(){Id = 2, Name = "Html"}
+        };
+        public JsonResult Courses()
+        {
+            return Json(CoursesData, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
